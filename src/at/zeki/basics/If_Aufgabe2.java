d1318ccce3cf9ff6b09fd8ef509a279fdcbe8745
Zeki57 <zeki.aksoy@student.htldornbirn.at>
2022-03-17 16:28:24 +0100

    If-Aufgabe2

---
+++ src/at/zeki/basics/If_Aufgabe2.java
@@ -0,0 +1,10 @@
+package at.zeki.basics;
+import java.util.Random;
+
+public class If_Aufgabe2 {
+    public static void main(String[]args){
+        Random random = new Random();
+        int randomNumber = random.nextInt(100);
+
+    }
+}
