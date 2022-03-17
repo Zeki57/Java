e5f8e61a3954c2cc9d9561e4b80bb0f2c5dacaa6
Zeki57 <zeki.aksoy@student.htldornbirn.at>
2022-03-17 16:32:43 +0100

    If-Aufgabe2

--- src/at/zeki/basics/If_Aufgabe2.java
+++ src/at/zeki/basics/If_Aufgabe2.java
@@ -4,7 +4,11 @@ import java.util.Random;
 public class If_Aufgabe2 {
     public static void main(String[]args){
         Random random = new Random();
-        int randomNumber = random.nextInt(100);
+        int randomNumber1 = random.nextInt(100);
+        int randomNumber2 = random.nextInt(100);
 
+        if ((randomNumber1<randomNumber2) && (randomNumber1<50)){
+            System.out.println(Zahl);
+        }
     }
 }
