27455c91b97b16feb89a7eb899ecd0d21fb45f45
Zeki57 <zeki.aksoy@student.htldornbirn.at>
2022-03-17 15:45:51 +0100

    If_Bedinungen

---
+++ src/at/zeki/basics/If_bedinung.java
@@ -0,0 +1,16 @@
+package at.zeki.basics;
+
+import java.util.Random;
+
+public class If_bedinung {
+    public static void main(String[] agrs) {
+        Random random = new Random();
+        int randomNumber = random.nextInt(100);
+
+        if (randomNumber>50){
+
+        } else if (randomNumber<10){
+
+        }else
+    }
+}
