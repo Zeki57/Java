62d09ae4fb44e6d83bab7e72c96a7cf8f924a28f
Zeki57 <zeki.aksoy@student.htldornbirn.at>
2022-03-31 16:04:37 +0200

    Loop_Aufgabe1

---
+++ src/at/zeki/basics/Loop_Aufgabe1.java
@@ -0,0 +1,10 @@
+package at.zeki.basics;
+
+public class Loop_Aufgabe1 {
+    public static void main(String[]args){
+        for (int i = 1; i >100 ; i++) {
+            System.out.println("Hello" +i);
+            
+        }
+    }
+}
