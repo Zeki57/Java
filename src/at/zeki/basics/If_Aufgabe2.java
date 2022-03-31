f1c0970380f17f76dc31bb765a1f8ee508463d67
Zeki57 <zeki.aksoy@student.htldornbirn.at>
2022-03-31 15:36:29 +0200

    If_Aufgabe2

--- src/at/zeki/basics/If_Aufgabe2.java
+++ src/at/zeki/basics/If_Aufgabe2.java
@@ -8,7 +8,10 @@ public class If_Aufgabe2 {
         int randomNumber2 = random.nextInt(100);
 
         if ((randomNumber1<randomNumber2) && (randomNumber1<50)){
-            System.out.println();
-        }
-    }
+            System.out.println("Zahl 1 ist kleiner als Zahl 2 und Mini");
+        }else if ((randomNumber1>30) && (randomNumber2>30)){
+            System.out.println("Eine der beiden ist kleiner als 30");
+        }else if ((randomNumber1>50) && (randomNumber2!=50)){
+            System.out.println("Erster Zahl klein, Zeweite kein 5iger");
+    };
 }
