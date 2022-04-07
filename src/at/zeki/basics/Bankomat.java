033583c8692076f94519f052634c03ba89f7fd56
Zeki57 <zeki.aksoy@student.htldornbirn.at>
2022-04-07 15:49:30 +0200

    Bankomat

---
+++ src/at/zeki/basics/Bankomat.java
@@ -0,0 +1,28 @@
+package at.zeki.basics;
+
+import java.util.Scanner;
+
+public class Bankomat {
+
+    static int input = 0;
+    static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[]agrs){
+
+        boolean exit = false;
+
+        int balance = 0;
+
+        while (!exit){
+            System.out.println();
+            System.out.println("Wählen sie eine option aus:");
+            System.out.println("geben sie 1 für Einzahlen ein:");
+            System.out.println("2 für Abheben:");
+            System.out.println("3 für Kontostand einzusehen:");
+            System.out.println("4 um es zu beenden:");
+            System.out.println();
+
+            input ("Bitte geben sie eine der oben gennante Zahlen ein.")
+        }
+    }
+}
