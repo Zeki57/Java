936ea29f67feccb7fbedfc165f1887d972b051a2
zeki57 <zeki.aksoy05@gmail.com>
2023-01-10 10:16:21 +0100

    OO_Car_Erweitern

---
+++ src/at/zeki/oo/Engine.java
@@ -0,0 +1,34 @@
+package at.zeki.oo;
+
+public class Engine {
+    public enum TYPE {GAS, DIESEL}
+
+    private double fuelConsumption;
+    private double consumptionFactor = 1;
+    private double horsePower;
+
+    public Engine(double fuelConsumption, int horsePower) {
+        this.fuelConsumption = fuelConsumption;
+        this.horsePower = horsePower;
+    }
+
+    public double getHorsePower() {
+        return horsePower;
+    }
+
+    public double getFuelConsumption() {
+        return fuelConsumption;
+    }
+
+    public void setFuelConsumption(double fuelConsumption) {
+        this.fuelConsumption = fuelConsumption;
+    }
+
+    public double getConsumptionFactor() {
+        return consumptionFactor;
+    }
+
+    public void setConsumptionFactor(double consumptionFactor) {
+        this.consumptionFactor = consumptionFactor;
+    }
+}
